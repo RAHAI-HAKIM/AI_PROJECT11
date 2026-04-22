40e2cb2214a81effac10198c9684408cf908861a
RAHAI-Abdelhakim <mohamed-abdelhakim.rahai@ensia.edu.dz>
2026-04-22 19:07:04 +0100

    The initia

--- optimizer.py
+++ optimizer.py
@@ -1,3 +1,19 @@
+
+#defines functions and returns optimized states
 class Optimizer:
-    #defines functions and returns optimized states
-    pass
+    def Simulated_Annealing(self, problem, initial_temp, cooling_rate, max_iterations):
+        # gets the problem's current state and returns an optimized one using SA
+        pass
+
+    def Hill_Climbing(self, problem, strategy="steepest"):
+        # gets the problem's current state and returns an optimized one using the choosen HC variant
+        pass
+
+    def Random_Restart_Hill_Climbing(self, problem, base_strategy="steepest", num_restarts=50):
+        # calls the choosen HC variant to optimize -num_restart- randomely choosen valid states and selects the best
+        pass
+
+    def Tabu_Search(self, problem):
+        # gets the problem's current state and returns an optimized one using tabu search
+        pass
+
--- problem.py
+++ problem.py
@@ -1,26 +1,52 @@
+
+# This is the class that implements main problem method, specific data-driven problems will enhirit from it 
 class Problem:
-    #defines a universisty event scheduling problem
     pass
 
 
 class Constraints:
-    # includes the constraints functions
+    # includes the constraints functions, seperated for clean structure
+    pass
 
-
+# ensia specific problem class
 class EnsiaProblem(Problem):
-    def __init__(self):
+    def __init__(self, dataset, cspmethod="global_search"):
         super().__init__()
 
-        self.event_ids = set()
-        self.roomids = set()
+        # load data elements
+        data = self.load_data(dataset)
+        self.events = data[0]
+        self.rooms = data[1]
+        # access data elements by their id
+        self.events_by_id = {e["id"]: e for e in self.events}
+        self.rooms_by_id  = {r["id"]:  r for r in self.rooms}
+
+        # fill the (room, time) tuple, assuming time is a number from 0-29
+        slots = []
+        for r in self.rooms:
+            for t in range(30):
+                slots.append((r["id"], t))
+        self.slots = slots
+        
+        # get constraint list -not handled yet-
         self.hard_constraints_list = {con_name: (function)}
         self.soft_constraints_list = {con_name: (function, weight)}
-        self.state = self.generate_random_state() #eventid -> (roomid, timeslot(day, slot)) # get timeslot from dataset
+
+        # the state of the problem is a dict in the form:
+        #  eventid -> (roomid, timeslot(day, slot)) 
+        if cspmethod == "local_search": 
+            state = self.generate_random_state() # generate a random assignment that might violate hard constraints
+            state = self.enhance(state) # does local search csp to resolve all hard constraints
+            self.state = state
+        else:
+            if cspmethod != "global_search": print("invalid csp method, redirecting to GS csp ...\n")
+            self.state = self.generate_valid_state()
+        
 
         
 
     def load_data(self, filename):
-        pass # fills the sets and clears the old ones
+        pass # returnes a list of 2 lists having events, rooms 
 
 
     def generate_valid_state(self): 
@@ -33,14 +59,19 @@ class EnsiaProblem(Problem):
         pass
 
     def generate_random_state(self):
-        # local search
+        # random assignment, not respecting any constraints
         pass
+    
+    def enhance(state):
+        # this method is used after generating a random state to resolve constraints using local search
+        padd
 
-    def generate_neighbors(state, size=10): # returns at most size states
+    def generate_neighbors(state, size=50): # returns at most size states
         pass
     def move_operator(self):
         return self.generate_neighbors(1)
     
     def evaluate(self, state):
-        # returns float
+        # returnes the cost of a state, may need other constraint-specific methodes
+        pass
 
